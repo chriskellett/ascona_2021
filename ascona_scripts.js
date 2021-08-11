ca7f6a34f9e0eae43f3c4cf979111bc3c122f1b6
Chris Kellett <53143184+chriskellett@users.noreply.github.com>
2021-08-11 19:14:19 +0100

    Update ascona_scripts.js

--- ascona_scripts.js
+++ ascona_scripts.js
@@ -1,4 +1,7 @@
 $(document).ready( function () {
   vacNumber = $('[data-name=job_title]').length;
   $('.vac-count').text(vacNumber);
+  
+  locNumber = $('[data-name=loc_title]').length;
+  $('[data-name=loc_count]').text(vacNumber);
 });
